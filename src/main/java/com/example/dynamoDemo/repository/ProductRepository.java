de2981aa2da1746f48306ae6e2d1634a8eb82a84
nislamovs <nizami.islamovs@1nce.com>
2022-09-24 13:43:14 +0300

    Implementing db repository using aws sdk v2.

--- src/main/java/com/example/dynamoDemo/repository/ProductRepository.java
+++ src/main/java/com/example/dynamoDemo/repository/ProductRepository.java
@@ -1,22 +1,14 @@
 package com.example.dynamoDemo.repository;
 
 import com.example.dynamoDemo.models.Product;
-import java.util.List;
-import org.socialsignin.spring.data.dynamodb.repository.DynamoDBPagingAndSortingRepository;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScanCount;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@EnableScan
-@EnableScanCount
-public interface ProductRepository extends DynamoDBPagingAndSortingRepository<Product, String> {
+public interface ProductRepository extends DynamoDbRepository<Product>{
 
-    long countByProductCategory(String productCategory);
-
-    List<Product> findAllByProductCategory(String productCategory);
-
-    Page<Product> findAll(Pageable pageable);
+        long countByProductCategory(String productCategory);
+//
+//    List<Product> findAllByProductCategory(String productCategory);
+//
+//    Page<Product> findAll(Pageable pageable);
 }
